7b12fc4b0c840113b0d442a39ce7b28bfb0cb865
Recentlystarted <100480668+Recentlystarted@users.noreply.github.com>
2026-04-20 01:05:17 +0530

    Remove test/debug files

--- check_api.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-s = requests.Session()
-r = s.post("http://127.0.0.1:8080/api/login", json={"password": "#@j!F@ruk$0902"})
-print("Login:", r.status_code, r.json())
-
-r = s.get("http://127.0.0.1:8080/api/phones")
-for p in r.json()["phones"]:
-    print(f"  {p['id']}: status={p['status']}, port={p['tunnel_port']}")
